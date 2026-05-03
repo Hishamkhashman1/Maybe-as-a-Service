654e623845d19e0eed9e792bc70f08b2d531bbbf
Hisham <hisham.khashman@gmail.com>
2026-05-03 19:10:33 +0900

    feat: added skeleton for endpoints

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,5 +5,20 @@ from dotenv import load_dotenv
 
 print("loaded dependencies")
 
+load_dotenv()
+
+database_url = os.getenv("database_url")
+
+engine = create_engine(database_url)
+
+app = FastAPI()
+
+@app.GET("/")
+async def root()
+#return frontend landing page
+
+@app.get("/maybe{maybe_id}")
+# return a random maybe when requested
+
 
 
